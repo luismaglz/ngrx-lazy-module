31d9b67322f342a8bc21c0102a291a47d83cf52a
luismaglz <luismaglz@live.com>
2022-07-10 22:54:18 -0600

    3 files modified

--- src/store/store1/store1.module.ts
+++ src/store/store1/store1.module.ts
@@ -1,9 +1,26 @@
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+import { ActionReducer } from '@ngrx/store/src';
+import { localStorageSync } from 'ngrx-store-localstorage/lib';
 import { reducer1 } from './reducer';
 
+export function sessionStorageSyncReducer(
+  reducer: ActionReducer<any>
+): ActionReducer<any> {
+  return localStorageSync({
+    keys: ['store1'],
+    storage: sessionStorage,
+    rehydrate: true,
+    restoreDates: false,
+  })(reducer);
+}
+
 @NgModule({
-  imports: [StoreModule.forFeature('store1', reducer1)],
+  imports: [
+    StoreModule.forFeature('store1', reducer1, {
+      metaReducers: [sessionStorageSyncReducer],
+    }),
+  ],
   declarations: [],
   providers: [],
 })
--- src/store/store2/store2.module.ts
+++ src/store/store2/store2.module.ts
@@ -1,9 +1,26 @@
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+import { ActionReducer } from '@ngrx/store/src';
+import { localStorageSync } from 'ngrx-store-localstorage/lib';
 import { reducer2 } from './reducer2';
 
+export function sessionStorageSyncReducer(
+  reducer: ActionReducer<any>
+): ActionReducer<any> {
+  return localStorageSync({
+    keys: ['store2'],
+    storage: sessionStorage,
+    rehydrate: true,
+    restoreDates: false,
+  })(reducer);
+}
+
 @NgModule({
-  imports: [StoreModule.forFeature('store2', reducer2)],
+  imports: [
+    StoreModule.forFeature('store2', reducer2, {
+      metaReducers: [sessionStorageSyncReducer],
+    }),
+  ],
   declarations: [],
   providers: [],
 })
